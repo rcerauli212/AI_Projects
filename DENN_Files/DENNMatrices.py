7c63d7cb9d7f1c04a4ef330a61d35720e5e1214d
rcerauli212 <ryan.cerauli@gmail.com>
2025-10-27 17:29:20 -0400

    Update DENNMatrices.py

--- DENN_Files/DENNMatrices.py
+++ DENN_Files/DENNMatrices.py
@@ -1,1 +1,5 @@
+# Written by Ryan Cerauli for the DAN Research Program headed by Anthony F. Beavers @ Indiana University. Copyright 2024. 
+# See https://www.afbeavers.net/drg for more information
 
+# This file will hold the weight sets for the solved DENN weights as well as the input weights. These can be called back on later so one does
+# have to retrain the network. The comments here will be deleted when sending over the weibght sets from the DAN->DENN_Neural_Network.py
